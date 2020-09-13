4d08760f9bd2c38be289068f865cddbff6e9f8a5
DensetsuOne <densetsu2015@mail.ru>
2020-09-13 19:13:38 +0700

    half mysqli

--- index.php
+++ index.php
@@ -18,7 +18,14 @@ $link = mysqli_connect($connect->host, $connect->user, $connect->password, $conn
     <input type="submit" name='button2'>
     </form>
     <?php
-
+    
+    // COUNT 
+    $query = "SELECT COUNT(*) as count FROM testie WHERE id>0";
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
+    $count = mysqli_fetch_assoc($result);
+    
+    var_dump($count);
+    
     //PROCEDURE DATABASE;
 
     // $sql = "CREATE DATABASE testbase_2";
@@ -42,16 +49,44 @@ $link = mysqli_connect($connect->host, $connect->user, $connect->password, $conn
     $button2 = $_POST['button2'];
     mysqli_query($link, "SET NAMES'utf8'");
     if(isset($button) == 1){
-    $query = "INSERT INTO testie SET names='Руссич', age='300', salary='5000'";
+    $query = "INSERT INTO testie (names, age, salary) VALUES ('Михаил', 45, 2)";
     mysqli_query($link,$query);
     }
 
-    if(isset($button2) == 1){
-        $query = "SELECT * FROM testie WHERE names='руссич'";
-       $result = mysqli_query($link, $query);
-       for($data = []; $row = mysqli_fetch_assoc($result); $data = $row);
-       var_dump($data);
-    }
+        //SELECT FOR DATABASE FROM TESTIE
+    // if(isset($button2) == 1){
+    //     $query = "SELECT * FROM testie WHERE names='руссич'";
+    //    $result = mysqli_query($link, $query);
+    //    for($data = []; $row = mysqli_fetch_assoc($result); $data = $row);
+    //    var_dump($data);
+    // }
+
+    //DELETED FOR DATABASE FROM TESTIE
+    // if(isset($button2) == 1){
+    //     $i = 40;
+    //     $a = $i + 7;
+    //     for($i; $i <= $a; $i++){
+    // $query = "DELETE FROM testie WHERE id='$i'";
+    // mysqli_query($link, $query);
+    // echo $i;
+    //     }
+    // }
+
+    //UPDATE FOR DATABASE FROM TESTIE
+    // if(isset($button2) == 1){
+    //     $query = "UPDATE testie SET names='항문 스틱을 통과' WHERE id='48'";
+    //     mysqli_query($link,$query);
+    // }
+
+    // ORDER BY 
+    // if(isset($button2) == 1){
+    //     $query = "SELECT * FROM testie WHERE id>0 ORDER BY age DESC";
+    //     $result = mysqli_query($link,$query);
+    //     for($data = []; $row = mysqli_fetch_assoc($result); $data = $row){
+    //     var_dump($data);
+    //     }
+    // }
+
     ?>
 </body>
 </html>
